b51a33f0b83eb0238dce5731d69f4fe47be99e2e
u7chan <t.unagami.dev@gmail.com>
2024-03-05 00:23:54 +0900

    fix: build error

--- src/app/admin/page.tsx
+++ src/app/admin/page.tsx
@@ -15,3 +15,5 @@ export default function Page() {
     </>
   )
 }
+
+export const dynamic = 'force-dynamic'
--- src/app/main/page.tsx
+++ src/app/main/page.tsx
@@ -92,3 +92,5 @@ export default async function Home() {
     </main>
   )
 }
+
+export const dynamic = 'force-dynamic'
